ecd75911b8899443551ee61e4542f6c1a4a2873d
Jeffrey Luo <77336910+lunarsceals@users.noreply.github.com>
2024-12-16 01:19:49 -0500

    Add flipCard tailwind plugin to handle flip animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
+import { transform } from "typescript";
+
+const plugin = require("tailwindcss/plugin");
+
+const flipCard = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".rotate-x-180": {
+      transform: "rotateX(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective-1000": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisbility: "hidden",
+    },
+  });
+});
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -15,5 +40,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [flipCard],
 };
