5a8fb215bc26423e0da0026fec64968c596d0d86
Jeffrey Luo <77336910+lunarsceals@users.noreply.github.com>
2024-12-16 01:25:22 -0500

    Add height props to Character component

--- frontend/src/component/card/Character.tsx
+++ frontend/src/component/card/Character.tsx
@@ -3,31 +3,31 @@ import HanziWriter from "hanzi-writer";
 
 interface CharacterProps {
   char: string;
+  width: number;
+  height: number;
 }
 
 //Creates HanziWriter instance
-const Character = (prop: CharacterProps) => {
+const Character = ({ char, width, height }: CharacterProps) => {
   //The state of the character needs to be retained between rerenders
   const characterRef = useRef<HTMLSpanElement | null>(null);
   const writerRef = useRef<HanziWriter | null>(null);
-
   useEffect(() => {
     /*
-      writerRef is here to keep track of the existance of a HanziWriter instance. 
-      Without it, when the componenet rerenders, a new instance will be created, 
-      and there will be duplicates. 
+      writerRef is here to keep track of the existance of a HanziWriter instance.
+      Without it, when the componenet rerenders, a new instance will be created,
+      and there will be duplicates.
     */
     if (characterRef.current && !writerRef.current) {
-      writerRef.current = HanziWriter.create(characterRef.current, prop.char, {
-        width: 70,
-        height: 70,
-        padding: 5,
+      writerRef.current = HanziWriter.create(characterRef.current, char, {
+        width,
+        height,
+        padding: 0,
       });
     } else if (writerRef.current) {
-      writerRef.current.setCharacter(prop.char);
+      writerRef.current.setCharacter(char);
     }
-  }, [prop.char]);
-
+  }, [char, width, height]);
   return <span ref={characterRef}></span>;
 };
 
